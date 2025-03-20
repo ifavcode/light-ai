d1a5dbf0a0746267b9f4892ff9d8645b78d94e15
emme <1308003218@qq.com>
2025-03-20 09:12:57 +0800

    补充sql

---
+++ light_ai.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : 47.99.113.58
+ Source Server Type    : MySQL
+ Source Server Version : 50650 (5.6.50-log)
+ Source Host           : 47.99.113.58:3306
+ Source Schema         : light_ai
+
+ Target Server Type    : MySQL
+ Target Server Version : 50650 (5.6.50-log)
+ File Encoding         : 65001
+
+ Date: 20/03/2025 09:12:22
+*/
+
